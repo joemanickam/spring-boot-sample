c67c7db8096962968fa5db6360aecb897c6679b6
joemanickam <joemanickam.gmail.com>
2021-03-21 20:09:26 +0000

    initial origin

---
+++ src/main/java/com/bootdemo/spring_boot_demo_sts/App.java
@@ -0,0 +1,13 @@
+package com.bootdemo.spring_boot_demo_sts;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/spring/demo/SpringBootDemoSts.java
@@ -0,0 +1,16 @@
+package com.spring.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoSts {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(SpringBootDemoSts.class, args);
+		
+
+	}
+
+}
